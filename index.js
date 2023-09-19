15bb9cca65e5a43ff03000508688c93d061f4df9
venkateshshyamala <venkat.mai938@gmail.com>
2023-09-19 19:48:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,91 @@
+let flag1=1;
+function para1()
+{
+    if(flag1==1)
+    {
+        flag1=0;
+        document.getElementById("dd1").style.borderRadius="40px";
+        document.getElementById("btn1Icon1").style.display="none";
+        document.getElementById("btn1Icon2").style.display="block";
+        document.getElementById("res1").style.display="block";
+
+    }
+    else
+    {
+        flag1=1;
+        document.getElementById("dd1").style.borderRadius="100px";
+        document.getElementById("btn1Icon2").style.display="none";
+        document.getElementById("btn1Icon1").style.display="block";
+        document.getElementById("res1").style.display="none";
+
+    }
+}
+
+let flag2=1;
+function para2()
+{
+    if(flag2==1)
+    {
+        flag2=0;
+        document.getElementById("dd2").style.borderRadius="40px";
+        document.getElementById("btn2Icon1").style.display="none";
+        document.getElementById("btn2Icon2").style.display="block";
+        document.getElementById("res2").style.display="block";
+
+    }
+    else
+    {
+        flag2=1;
+        document.getElementById("dd2").style.borderRadius="100px";
+        document.getElementById("btn2Icon2").style.display="none";
+        document.getElementById("btn2Icon1").style.display="block";
+        document.getElementById("res2").style.display="none";
+
+    }
+}
+
+let flag3=1;
+function para3()
+{
+    if(flag3==1)
+    {
+        flag3=0;
+        document.getElementById("dd3").style.borderRadius="40px";
+        document.getElementById("btn3Icon1").style.display="none";
+        document.getElementById("btn3Icon2").style.display="block";
+        document.getElementById("res3").style.display="block";
+
+    }
+    else
+    {
+        flag3=1;
+        document.getElementById("dd3").style.borderRadius="100px";
+        document.getElementById("btn3Icon2").style.display="none";
+        document.getElementById("btn3Icon1").style.display="block";
+        document.getElementById("res3").style.display="none";
+
+    }
+}
+
+let flag4=1;
+function para4()
+{
+    if(flag4==1)
+    {
+        flag4=0;
+        document.getElementById("dd4").style.borderRadius="40px";
+        document.getElementById("btn4Icon1").style.display="none";
+        document.getElementById("btn4Icon2").style.display="block";
+        document.getElementById("res4").style.display="block";
+
+    }
+    else
+    {
+        flag4=1;
+        document.getElementById("dd4").style.borderRadius="100px";
+        document.getElementById("btn4Icon2").style.display="none";
+        document.getElementById("btn4Icon1").style.display="block";
+        document.getElementById("res4").style.display="none";
+
+    }
+}
